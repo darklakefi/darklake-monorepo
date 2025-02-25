1394330e14e067be41963ba8645f873b7eeffcc7
poocart <7067483+poocart@users.noreply.github.com>
2025-02-25 10:11:39 +0200

    added text styles

--- web-new/src/components/Footer.tsx
+++ web-new/src/components/Footer.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 const Footer = () => {
   return (
     <footer className="flex flex-row justify-between items-center pb-[80px]">
-      <button className="uppercase text-brand-30 underline active-hover-opacity">What is MEV?</button>
+      <button className="uppercase text-brand-30 text-link-1">What is MEV?</button>
       <div className="flex flex-row gap-x-[40px]">
         {socials.map((social) => (
           <a href={social.url} key={social.title} target="_blank" rel="noreferrer" className="active-hover-opacity">
