665bdfbeebcd314e66226722afce602966b173fa
poocart <7067483+poocart@users.noreply.github.com>
2025-03-07 10:30:19 +0200

    added attack breakdown mobile fixes

--- apps/web/src/components/Modal/AttackBreakdownModal.tsx
+++ apps/web/src/components/Modal/AttackBreakdownModal.tsx
@@ -15,7 +15,7 @@ export default function AttackBreakdownModal(props: AttackBreakdownModalProps) {
 
   return (
     <Modal title="Attack breakdown" {...props}>
-      <div className="md:w-[960px]">
+      <div className="lg:w-[960px]">
         <div className="uppercase text-lg leading-6 text-brand-30 mb-6">
           <p>Token: {mevAttack.tokenName}</p>
           <p>Date: {format(mevAttack.timestamp, "yyyy-MM-dd hh:mm OOO")}</p>
@@ -36,7 +36,7 @@ export default function AttackBreakdownModal(props: AttackBreakdownModalProps) {
           ))}
         </div>
         <div className="relative">
-          <span className="w-px h-full absolute left-[11.5px] top-0 bg-brand-50" />
+          <span className="w-px h-full absolute left-[11.5px] max-md:hidden top-0 bg-brand-50" />
           {[
             {
               title: "Frontrun Attack",
@@ -68,17 +68,22 @@ export default function AttackBreakdownModal(props: AttackBreakdownModalProps) {
             <div
               key={`${row.title}-${row.transaction.address}${row.transaction.address}-${row.transaction.signature}`}
               className={cn(
-                "flex flex-row items-center w-full gap-x-[7px] mb-6 relative",
+                "md:flex flex-row items-center w-full gap-x-[7px] md:mb-6 relative",
                 "uppercase text-lg leading-5",
                 "group",
               )}
             >
-              <span className="hidden group-first-of-type:block absolute top-0 left-0 bg-brand-70 h-1/2 w-full" />
-              <span className="hidden group-last-of-type:block absolute bottom-0 left-0 bg-brand-70 h-1/2 w-full" />
-              <div className="text-brand-20 w-[264px] flex flex-row items-center py-[18px] bg-brand-70 relative">
+              <span className="hidden md:group-first-of-type:block absolute top-0 left-0 bg-brand-70 h-1/2 w-full" />
+              <span className="hidden md:group-last-of-type:block absolute bottom-0 left-0 bg-brand-70 h-1/2 w-full" />
+              <div
+                className={cn(
+                  "text-brand-20 md:w-[264px]",
+                  "flex flex-row items-center justify-center py-[18px] bg-brand-70 relative",
+                )}
+              >
                 <i className={cn("text-[22px] hn mr-4", row.iconClassName)} />
                 <p>{row.title}</p>
-                <span className="ml-[5px] h-px w-[74px] bg-brand-50 flex-1" />
+                <span className="max-md:hidden ml-[5px] h-px w-[74px] bg-brand-50 flex-1" />
               </div>
               <div className=" text-brand-30 border border-brand-60 p-5 flex-1 relative">
                 <p className="text-brand-20">{row.details}</p>
@@ -87,6 +92,7 @@ export default function AttackBreakdownModal(props: AttackBreakdownModalProps) {
                   View TX
                 </a>
               </div>
+              <span className="block group-last-of-type:hidden md:hidden w-px mt-4 h-8 mx-auto bg-brand-50" />
             </div>
           ))}
         </div>
