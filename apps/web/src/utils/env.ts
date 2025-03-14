7bc82773183b44f68868bc1be783bc805372b672
poocart <7067483+poocart@users.noreply.github.com>
2025-03-14 11:36:06 +0200

    vercel system env chore

--- apps/web/src/utils/env.ts
+++ apps/web/src/utils/env.ts
@@ -1,9 +1,6 @@
 export const getSiteUrl = () => {
-  if (process.env.NEXT_PUBLIC_VERCEL && !process.env.NEXT_PUBLIC_SITE_URL) {
-    const hostname =
-      process.env.NEXT_PUBLIC_VERCEL_ENV === "production"
-        ? process.env.NEXT_PUBLIC_VERCEL_URL
-        : process.env.NEXT_PUBLIC_VERCEL_BRANCH_URL;
+  if (process.env.VERCEL && !process.env.NEXT_PUBLIC_SITE_URL) {
+    const hostname = process.env.VERCEL_ENV === "production" ? process.env.VERCEL_URL : process.env.VERCEL_BRANCH_URL;
     return `https://${hostname}`;
   }
 
