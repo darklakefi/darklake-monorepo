86c56fb628ca18324636f26fbdaa7a14424384e8
poocart <7067483+poocart@users.noreply.github.com>
2025-03-28 00:48:05 +0200

    dompurify chore

--- apps/web/src/components/AddressMevLookup.tsx
+++ apps/web/src/components/AddressMevLookup.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useRef, useState } from "react";
 import Link from "next/link";
-import DOMPurify from "dompurify";
+import DOMPurify from "isomorphic-dompurify";
 import { useRouter } from "next/navigation";
 
 import { pasteFromClipboard } from "@/utils/browser";
