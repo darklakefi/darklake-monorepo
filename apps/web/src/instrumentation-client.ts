5382c3fee540e7a8b0abcf8692daf6b65d0a6175
Lewis Flude <lewis@lewisflude.com>
2025-03-28 11:55:11 +0000

    Address feedback

--- apps/web/src/instrumentation-client.ts
+++ apps/web/src/instrumentation-client.ts
@@ -5,7 +5,7 @@
 import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
-  dsn: "https://773fd9bc5b4d5d1ff4d35335fce01b3c@o4508075926618112.ingest.de.sentry.io/4508075930484816",
+  dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
 
   // Add optional integrations for additional features
   integrations: [Sentry.replayIntegration()],
--- apps/web/src/instrumentation.ts
+++ apps/web/src/instrumentation.ts
@@ -1,26 +1,10 @@
 import * as Sentry from "@sentry/nextjs";
 
 export async function register() {
-  if (process.env.NEXT_RUNTIME === "nodejs") {
+  if ((process.env.NEXT_RUNTIME === "nodejs" || process.env.NEXT_RUNTIME === "edge") && process.env.SENTRY_DSN) {
     Sentry.init({
-      dsn: "https://773fd9bc5b4d5d1ff4d35335fce01b3c@o4508075926618112.ingest.de.sentry.io/4508075930484816",
-
-      // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+      dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
       tracesSampleRate: 1,
-
-      // Setting this option to true will print useful information to the console while you're setting up Sentry.
-      debug: false,
-    });
-  }
-
-  if (process.env.NEXT_RUNTIME === "edge") {
-    Sentry.init({
-      dsn: "https://773fd9bc5b4d5d1ff4d35335fce01b3c@o4508075926618112.ingest.de.sentry.io/4508075930484816",
-
-      // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
-      tracesSampleRate: 1,
-
-      // Setting this option to true will print useful information to the console while you're setting up Sentry.
       debug: false,
     });
   }
