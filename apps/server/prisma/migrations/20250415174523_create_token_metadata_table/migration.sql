785541d0a2497a94562ec1a6ad65ad2df0398bb4
Felix Hoang <hdcompany123@gmail.com>
2025-04-28 17:29:11 +0700

    feat: add additional indexes to sandwich_events table

--- apps/server/prisma/migrations/20250415174523_create_token_metadata_table/migration.sql
+++ apps/server/prisma/migrations/20250415174523_create_token_metadata_table/migration.sql
@@ -13,3 +13,9 @@ CREATE TABLE "token_metadata" (
 
 -- CreateIndex
 CREATE INDEX "sandwich_events_token_address_idx" ON "sandwich_events"("token_address");
+
+-- CreateIndex
+CREATE INDEX "sandwich_events_occurred_at_idx" ON "sandwich_events"("occurred_at");
+
+-- CreateIndex
+CREATE INDEX "sandwich_events_victim_address_occurred_at_idx" ON "sandwich_events"("victim_address", "occurred_at");
