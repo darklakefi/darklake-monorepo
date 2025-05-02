8a673e56dd017f252b5005e39c6ab9a4457afcc0
Felix Hoang <hdcompany123@gmail.com>
2025-05-02 10:09:49 +0700

    feat: add new SVG logos for Darklake and update header component

--- apps/web/src/components/Header.tsx
+++ apps/web/src/components/Header.tsx
@@ -21,9 +21,8 @@ const Header = () => {
     <header className="flex flex-row justify-between items-center z-20 py-16">
       <div className="flex items-center justify-between gap-x-[11px]">
         <Link href="/" title="Darklake.fi" className="active:opacity-80">
-          <Image src="/images/logo-h-darklake.png" alt="darklake logo" height={24} width={147} />
+          <Image src="/images/logo-h-darklake-beta.svg" alt="darklake logo" height={24} width={213} />
         </Link>
-        <p className="font-primary text-lg text-brand-30 select-none">BETA</p>
       </div>
       {isResultsPage && lookupAddress ? (
         <ConnectedWalletDropdown
