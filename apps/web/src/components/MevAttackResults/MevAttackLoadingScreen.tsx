9cb286a5becddbb76a50a4eb8e2b9d7a63b1f5cf
Felix Hoang <hdcompany123@gmail.com>
2025-05-12 16:56:24 +0700

    feat: dot dot dot animation in loading screen

--- apps/web/src/components/MevAttackResults/MevAttackLoadingScreen.tsx
+++ apps/web/src/components/MevAttackResults/MevAttackLoadingScreen.tsx
@@ -8,7 +8,9 @@ export function MevAttackLoadingScreen() {
           <h1 className="font-primary text-3xl leading-7 text-brand-30 mb-8">
             Analyzing The Blocks
             <br />
-            <span className="text-brand-20">This might take a few seconds.</span>
+            <span className="text-brand-20">This might take a few seconds
+              <span className="animate-dots"></span>
+            </span>
           </h1>
         </div>
         <div className="flex items-center justify-end flex-1">
